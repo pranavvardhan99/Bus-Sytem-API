c6b25ae7e7671ba4fbdfbcce72910232278f2f38
pranavvardhan99 <114789564+pranavvardhan99@users.noreply.github.com>
2023-05-07 14:18:53 +0530

    Update bus1.py

--- bus1.py
+++ bus1.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import psycopg2
+import logging
 
 app = FastAPI()
 
 # Postgres connection details
 DB_HOST = 'localhost'
 DB_PORT = '5432'
-DB_NAME = 'app pro'
+DB_NAME = 'FinalBus'
 DB_USER = 'postgres'
 DB_PASSWORD = '310D60c9@'
 
@@ -17,6 +18,14 @@ DB_PASSWORD = '310D60c9@'
 class TransportRegistrationRequest(BaseModel):
     userid: str
     semstr_details_semester_sub_id: str
+
+
+# Set up logger
+logging.basicConfig(
+    filename='app.log',
+    level=logging.ERROR,
+    format='%(asctime)s %(levelname)s %(message)s'
+)
 
 # Define route to handle incoming requests
 
@@ -37,7 +46,8 @@ def get_transport_registration(request: TransportRegistrationRequest):
             password=DB_PASSWORD
         )
     except psycopg2.Error as e:
-        # Raise HTTPException if connection fails
+        # Log error and raise HTTPException if connection fails
+        logging.error('Failed to connect to database')
         raise HTTPException(
             status_code=500, detail='Failed to connect to database')
 
@@ -51,10 +61,11 @@ def get_transport_registration(request: TransportRegistrationRequest):
                 user_id, sem_sub_id,)
         )
     except psycopg2.Error as e:
-        # Rollback transaction and raise HTTPException if query fails
+        # Rollback transaction and log error, then raise HTTPException if query fails
         conn.rollback()
         cur.close()
         conn.close()
+        logging.error('Failed to execute query')
         raise HTTPException(status_code=500, detail='Failed to execute query')
 
     # Fetch column names
@@ -67,8 +78,41 @@ def get_transport_registration(request: TransportRegistrationRequest):
     cur.close()
     conn.close()
 
-    # Raise HTTPException if no rows were found
+    # If no rows were found, log error and return 404 response
     if len(rows) == 0:
+        try:
+            # Connect to Postgres
+            conn = psycopg2.connect(
+                host=DB_HOST,
+                port=DB_PORT,
+                dbname=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD
+            )
+
+            # Open a cursor to perform database operations
+            cur = conn.cursor()
+
+            # Execute SQL query to insert error details into bus_qr_scan_log table
+            cur.execute(
+                "INSERT INTO transport.bus_qr_scan_log (log_userid, semstr_details_semester_sub_id) VALUES (%s, %s)",
+                (user_id, sem_sub_id,)
+            )
+
+            # Commit transaction and close database connection
+            conn.commit()
+            cur.close()
+            conn.close()
+
+        except psycopg2.Error as e:
+            # Rollback transaction, log error, and raise HTTPException if query fails
+            conn.rollback()
+            cur.close()
+            conn.close()
+            logging.error('Failed to insert error details into database')
+            raise HTTPException(
+                status_code=500, detail='Failed to insert error details into database')
+
         raise HTTPException(
             status_code=404, detail='No records found for the given user id and semester subject id'
         )
